0c8ec540cbd7fd09a85b8170a4a34044bc6bffad
Igor Kulman <igor@kulman.sk>
2015-05-03 16:55:06 +0200

    * init commit

---
+++ OpenALPR.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using OpenALPR.Lib;
+
+namespace OpenALPR.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                System.Console.WriteLine("Usage: OpenALPR.Console.exe imagePath");
+                return;
+            }
+
+            var lib = new OpenALPRLib();
+            var res = lib.GetBestMatch(Country.EU, args[0]);
+            System.Console.WriteLine(res);
+        }
+    }
+}
